9405d867a5e661bd1975e2dfb5d60f63ff3ae930
eze1er <papaezechiel@gmail.com>
2021-11-30 23:59:42 +0000

    Commit after modif

---
+++ spinner.js
@@ -0,0 +1,18 @@
+setTimeout(() => {
+  process.stdout.write('\r|/-_/\ ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/ ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r- -');
+}, 500);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r /  '); 
+}, 700);
+
+// ... fill in the rest yourself ...
