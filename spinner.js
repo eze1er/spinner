74828d38b24f881f1bf36e363cd3f92cdd6001ee
eze1er <papaezechiel@gmail.com>
2021-12-01 00:10:46 +0000

    Commt for spinner2

--- spinner.js
+++ spinner.js
@@ -1,18 +1,6 @@
+const timer = 100;
 setTimeout(() => {
   process.stdout.write('\r|/-_/\ ');
-}, 100);
+}, timer);
+  timer += 200;
 
-setTimeout(() => {
-  process.stdout.write('\r/ ');
-}, 300);
-
-setTimeout(() => {
-  process.stdout.write('\r- -');
-}, 500);
-
-setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r /  '); 
-}, 700);
-
-// ... fill in the rest yourself ...
